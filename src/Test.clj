b9b50db59f90e2edefb55d5167a9f86d24be241b
ajp8868 <speedinglights@hotmail.co.uk>
2016-02-21 22:44:11 +0000

    Added loads to definitions, Method definitions too.
    Also changed the test file to allow for the strips-search.

--- src/Test.clj
+++ src/Test.clj
@@ -2,7 +2,7 @@
 (load-file "src/ops-search.clj")
 (load-file "src/wrappers.clj")
 (load-file "src/definitions.clj")
-;(load-file "strips-search.clj")
+(load-file "src/strips-search.clj")
 (load-file "src/planner.clj")
 (load-file "src/parsing.clj")
 
--- src/definitions.clj
+++ src/definitions.clj
@@ -178,9 +178,17 @@
        :txt  ((move side pusher to ?x))
        }
 
-     move-bottom-to
+     :move-bottom-to
      {
-
+       :name move-bottom-to
+       :achieves (at bp ?x)
+       :when ((:not (at bp ?x)))
+       :post ((at bp ?x))
+       :pre  ((:not (at bp ?x)))
+       :del  ((at bp ?y))
+       :add  ((at bp ?x))
+       :cmd  ((exec.move-to "B" ?y))
+       :txt  ((move bottom pusher to ?x))
        }
 
      :clearspace
@@ -274,6 +282,16 @@
        :txt (grasp! ?x off ?y at ?s)
        :cmd (pick-from ?s)
        }
+     move-arm
+     { :pre ( (hand retracted)
+              )
+       :del ( (at ?p ?x)
+              )
+       :add ( (at ?p ?y)
+              )
+       :txt (Moving ?p from ?x to ?y)
+       :cmd (move-pusher ?p ?y)
+       }
      push-down
      { :pre ( (holds ?x)
               (at ?dst ?s)
@@ -345,3 +363,80 @@
        :cmd (dispose)
        }})
 
+
+
+
+
+
+
+
+
+
+
+
+
+;====================================================
+; mapping for resolved parses to search goals, etc
+;====================================================
+
+(declare
+  goal apply-exec
+  extract-to-map clear-block-data
+  it-reference)
+
+(def default-block-spec '{color grey, size med, shape cube})
+
+
+(defmatch process-cmd []
+  ((grasp ?x)        :=> (set-atom! it-reference (? x))   (goal (mout '(holds ?x))))
+
+  ((put-on ?x ?y)    :=> (set-atom! it-reference (? x))   (goal (mout '(on ?x ?y))))
+
+  ((put-at ?x ?s)    :=> (set-atom! it-reference (? x))   (goal (mout '(at ?x ?s))))
+
+  ((move-hand-to ?s) :=> (apply-exec ('puton exec-ops) (mout '{s ?s})))
+
+  ((move-top-pusher-to ?y) :=> (apply-exec ('puton exec-ops) (mout '{s ?s})))
+
+  ((create ?x ?spec) :=>
+    (goal '(hand empty))
+    (set-atom! it-reference (? x))
+    (apply-exec ('create exec-ops)
+      (conj {'x (? x)}
+        (merge default-block-spec
+          (extract-to-map (? spec))))))
+
+  ((destroy ?x)      :=>
+    (goal (mout '(holds ?x)))
+    (apply-exec ('dispose exec-ops))
+    (set-atom! it-reference false))
+
+  ((reset)         :=> (clear-block-data) (nlogo-send "setup"))
+  ; ( ?x             :=> (ui-out :dbg 'ERROR '(unknown NetLogo request) (? x)))
+  )
+
+
+
+;================================
+; Netlogo comms & filters
+;================================
+
+
+(let [sizes '{small 5, med 7, large 9}
+      sp    " "
+      qt    "\""
+      str-qt   (fn[x] (str " \"" x "\" "))    ; wrap x in quotes
+      stack-no (fn[x] (apply str (rest (str x))))   ; strip "s" of stack name
+      ]
+
+
+  (defmatch nlogo-translate-cmd []
+    ((make ?nam ?obj ?size ?color)
+      :=> (str 'exec.make (str-qt (? nam)) (str-qt (? obj))
+            ((? size) sizes) (str-qt (? color))))
+    ((move-to ?s)   :=> (str 'exec.move-to sp (stack-no (? s))))
+    ((drop-at ?s)   :=> (str 'exec.drop-at sp (stack-no (? s))))
+    ((pick-from ?s) :=> (str 'exec.pick-from sp (stack-no (? s))))
+    ((move-pusher ?p ?y) :=> (str 'exec. sp (stack-no (? s))))
+    ( ?_            :=> (ui-out :dbg 'ERROR '(unknown NetLogo cmd)))
+    ))
